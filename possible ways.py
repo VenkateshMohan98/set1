325129a5d9ee1f9fb43b6ee7b296440b3cd92bf4
VenkateshMohan98 <51222591+VenkateshMohan98@users.noreply.github.com>
2019-07-17 14:37:10 +1000

    Update possible ways.py

--- possible ways.py
+++ possible ways.py
@@ -1,8 +1,10 @@
+
 v=int(input())
 k=list(map(int,input().split()))[:v]
 for i in range(len(k)):
-    for j in range(len(k)):
-         for vk in range(len(k)):
-               a=k[i]+k[j]
-                    if((a==k[vk]) and (i<j<vk)):
-                           print(k[i],k[j],k[vk])
+        for j in range(len(k)):
+                for vk in range(len(k)):
+                        a=k[i]+k[j]
+                        if((a==k[vk]) and (i<j<vk)):
+                                print(k[i],k[j],k[vk])
+
